fe787862bafcd206fbb3b5a270b956ceaac4eaaf
geroboog <geroboog3@outlook.com>
2017-09-11 11:48:34 +0800

    pythonWeb dao cache

--- service/ClassifyImageService.py
+++ service/ClassifyImageService.py
@@ -1,9 +1,12 @@
+import json
 from service.classify_image import *
+from util.ResponseUtil import ResponseUtil
 
 
 class ClassifyImageService(object):
     def __init__(self): pass
 
     def classifyImage(self, filename):
-        startClassify(filename)
-        return "xxxx"
+        result = startClassify(filename)
+
+        return json.dumps(ResponseUtil.getResponse(0, result))
